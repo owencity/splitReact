51c619ffbcac63782c604b96283c7d36e06586b0
owencity <skyko6530@naver.com>
2024-11-05 23:16:57 +0900

    useState

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,28 +1,26 @@
 import './App.css';
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-import Button from "./components/Button";
+import Bulb from './components/Bulb';
+import Counter from './components/Counter';
+// 리랜더링이 진행되는 상황 
+// 1. state 변경
+// 2. props 값을 받았을 때
+// 3. 부모 컴포넌트가 리랜더링되면 자식도 리랜더링 진행된다.
+
+
+
+
 function App() { // 부모 부모->자식 (계층) 
   // App -> 루트 컴포넌트 , 
-
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
-
+ 
+ 
   return (
     <>
-    <Button text={"메일"} color={"red"}/>
-    <Button text={"카페"}/>
-    <Button text={"블로그"}>
-      <Header/>
-      </Button>
-    </>
+      <Bulb/>
+      <Counter/>
+     </>
   )
 }
+
+// 화면을 다시그려 값을 반환-> 리랜더링
 
 export default App
--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,6 +1,15 @@
 const Button = ({text, color, children}) => {
+    // 이벤트 객체
+    const onClickButton = (e) => {
+        console.log(e);
+        console.log(text);
+    }
+    
     return (
-    <button style={{color : color}}>
+    <button 
+    onClick={onClickButton} // 콜백함수 전달하듯이 이름만
+    // onMouseEnter={onClickButton}
+    style={{color : color}}>
         {text} - {color.toUpperCase()}
         {children} 
         </button>
